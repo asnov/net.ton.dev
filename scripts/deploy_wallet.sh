bf72aced740ecb2a9efed2fc76effdb42d209822
Alex Novikov on mac-mini <alex@asnovikov.ru>
2020-09-30 22:55:16 -0300

    changed port;

--- scripts/deploy_wallet.sh
+++ scripts/deploy_wallet.sh
@@ -4,4 +4,4 @@ SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)
 # shellcheck source=env.sh
 . "${SCRIPT_DIR}/env.sh"
 
-"${TON_BUILD_DIR}/lite-client/lite-client" --verbosity 9 -p "${KEYS_DIR}/liteserver.pub" -a 127.0.0.1:7071 -rc "sendfile ${KEYS_DIR}/${HOSTNAME}-query.boc" -rc "quit"
+"${TON_BUILD_DIR}/lite-client/lite-client" --verbosity 9 -p "${KEYS_DIR}/liteserver.pub" -a 127.0.0.1:2021 -rc "sendfile ${KEYS_DIR}/${HOSTNAME}-query.boc" -rc "quit"
